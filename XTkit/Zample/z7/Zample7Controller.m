3c503a88342df330e7a7d4af69ec533add8f50ae
teason23 <akateason23@gmail.com>
2017-05-12 17:07:08 +0800

    a

---
+++ XTkit/Zample/z7/Zample7Controller.m
@@ -0,0 +1,40 @@
+//
+//  Zample7Controller.m
+//  XTkit
+//
+//  Created by teason23 on 2017/5/12.
+//  Copyright © 2017年 teason. All rights reserved.
+//
+
+#import "Zample7Controller.h"
+
+@interface Zample7Controller ()
+
+@end
+
+@implementation Zample7Controller
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad] ;
+    // Do any additional setup after loading the view.
+    self.title = @"rac" ;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
