6d5325bce3932c198ee74e94654541cb35552bdb
tianchen.xie <tianchen.xie@phicomm.com>
2017-06-15 17:07:22 +0800

    cmt image ad

--- XTkit/XTkit/category/UIImage+AddFunction.h
+++ XTkit/XTkit/category/UIImage+AddFunction.h
@@ -14,22 +14,22 @@
 
 #pragma mark -- 
 #pragma mark - style
-//1.裁剪圆形 + 边框
+//裁剪圆形 + 边框
 - (UIImage *)cutImageWithCircleWithBorderWidth:(CGFloat)margin
                             AndWithBorderColor:(UIColor *)borderColor ;
 
 + (UIImage *)image:(UIImage *)image
           rotation:(UIImageOrientation)orientation ;
 
-//2.裁剪正方
+//裁剪正方
 + (UIImage *)squareImageFromImage:(UIImage *)image
                      scaledToSize:(CGFloat)newSize ;
 
-//3.添加水印
+//添加水印
 - (UIImage *)imageWithWaterMask:(UIImage*)mask
                          inRect:(CGRect)rect;
 
-//4.缩略图thumbnail
+//缩略图thumbnail
 + (UIImage *)thumbnailWithImage:(UIImage *)image
                            size:(CGSize)asize;
 // 等比缩放
@@ -37,23 +37,31 @@
                        targetSize:(CGSize)size ;
 - (UIImage *)imageCompressForWidth:(UIImage *)sourceImage
                        targetWidth:(CGFloat)defineWidth ;
-// 压缩图片质量
+//压缩图片质量
 + (UIImage *)compressQualityWithOriginImage:(UIImage *)sourceImage ;
 
-// 相册获取
+//相册获取
 + (UIImage *)fetchFromLibrary:(ALAsset *)asset ;
 
-//5.拍完照片的自适应旋转(和相机一起用)
+//拍完照片的自适应旋转(和相机一起用)
 + (UIImage *)fixOrientation:(UIImage *)aImage;
 
-//6.颜色变图片
+//生成纯色图片
 + (UIImage *)imageWithColor:(UIColor *)color
                        size:(CGSize)size;
 
-//7.将UIView转成UIImage
+//改变图片颜色
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor ;
+
+- (UIImage *) imageWithGradientTintColor:(UIColor *)tintColor ;
+
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor blendMode:(CGBlendMode)blendMode ;
+
+
+//将UIView转成UIImage
 + (UIImage *)getImageFromView:(UIView *)theView ;
 
-//8.模糊
+//模糊
 - (UIImage *)blur ;
 
 - (UIImage *)boxblurImageWithBlur:(CGFloat)blur ;
