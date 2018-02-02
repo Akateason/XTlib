0c4f0e21c5c810f73b3879c869ddd6cd585ec5bf
Teason.xie <Teason.xie@cootek.cn>
2018-02-02 19:07:36 +0800

    add flow layout

---
+++ XTkit/XTkit/UIs/XTDrawerFlowLayout/XTDrawerFlowLayout.h
@@ -0,0 +1,15 @@
+//
+//  XTDrawerFlowLayout.h
+//  xtcPwd
+//
+//  Created by xtc on 2018/2/2.
+//  Copyright © 2018年 teason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XTDrawerFlowLayout : UICollectionViewFlowLayout
+
+@property (nonatomic, assign) CGFloat firstItemTransform;
+
+@end
