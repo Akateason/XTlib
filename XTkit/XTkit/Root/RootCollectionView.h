9e958cc2cf213e457b43c15a1c4006a084a30aa7
Teason.xie <Teason.xie@cootek.cn>
2018-02-07 14:15:10 +0800

    add root colleciton

---
+++ XTkit/XTkit/Root/RootCollectionView.h
@@ -0,0 +1,52 @@
+//
+//  RootCollectionView.h
+//  XTkit
+//
+//  Created by xtc on 2018/2/7.
+//  Copyright © 2018年 teason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootCollectionView ;
+
+@protocol RootCollectionViewDelegate <NSObject>
+@required
+- (void)collectionView:(RootCollectionView *)collection loadNew:(void(^)(void))endRefresh   ;
+@optional
+- (void)collectionView:(RootCollectionView *)collection loadMore:(void(^)(void))endRefresh  ;
+@end
+
+@interface RootCollectionView : UICollectionView
+// refresh delegate
+@property (nonatomic,weak) id <RootCollectionViewDelegate> xt_Delegate ;
+/**
+ REFRESH STYLE:
+ DEFAULT IS `NO`  -> ONLY GIF IMAGES , SHOW WORDS WHEN IT BECOMES `YES`
+ */
+@property (nonatomic) BOOL isShowRefreshDetail ;
+/**
+ is auto LOAD MORE:
+ DEFAULT IS `NO`  -> MANUALLY LOADING . AUTOMATICALLY LOAD WHEN IT BECOMES `YES`
+ */
+@property (nonatomic) BOOL isAutomaticallyLoadMore ;
+/**
+ PULL DOWN HEADER
+ */
+- (void)loadNewInfo ;
+/**
+ PULL DOWN HEADER
+ @param isBackGround    pull header in backgound or not .
+ */
+- (void)loadNewInfoInBackGround:(BOOL)isBackGround ;
+/**
+ prepareStyle
+ u can rewrite in subclass if needed
+ */
+- (void)prepareStyle ;
+/**
+ endRefresh header and footer if needed .
+ */
+- (void)endHeaderAndFooterRefresh ;
+
+@end
