e20fee67202ff7823dedf92e8cf75aa0a8758251
teason <teason.xie@cootek.cn>
2018-02-23 18:53:06 +0800

    log way

--- XTlib/XTlib/XTlib.h
+++ XTlib/XTlib/XTlib.h
@@ -9,6 +9,21 @@
 #ifndef XTlib_h
 #define XTlib_h
 
+#define xt_DEBUG    1
+
+#if xt_DEBUG
+#define NSLog(format, ...) do {                                             \
+fprintf(stderr, "<%s : %d> %s\n",                                           \
+[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],  \
+__LINE__, __func__);                                                        \
+(NSLog)((format), ##__VA_ARGS__);                                           \
+fprintf(stderr, "🏀🏀🏀🏀🏀\n");                                           \
+} while (0)
+
+#else
+#   define NSLog(...)
+#endif
+
 // header
 #import "ArchiveHeader.h"
 #import "KeyChainHeader.h"
