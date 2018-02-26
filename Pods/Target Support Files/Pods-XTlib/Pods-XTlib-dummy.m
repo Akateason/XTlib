62d039a1100e1bd0c63ad8f08d983dbcb98e4293
teason23 <akateason@qq.com>
2018-02-26 11:53:29 +0800

    移除src目录下所有文件的版本控制

--- Pods/Target Support Files/Pods-XTlib/Pods-XTlib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XTlib : NSObject
-@end
-@implementation PodsDummy_Pods_XTlib
-@end
--- Pods/Target Support Files/XTColor/XTColor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XTColor : NSObject
-@end
-@implementation PodsDummy_XTColor
-@end
