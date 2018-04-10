5aecef78fe66098ea2484ca034d6f2f74a3c5a4c
teason <teason.xie@cootek.cn>
2018-04-10 11:19:38 +0800

    update gradientBt

---
+++ XTlib/XTlib/UIs/GradientButton/GradientButton.h
@@ -0,0 +1,30 @@
+//
+//  GradientButton.h
+//  SwiftCall
+//
+//  Created by teason23 on 2018/3/27.
+//  Copyright © 2018年 weyl. All rights reserved.
+// ---------------------------------------------------------------- //
+//  e.g.
+//    GradientButton *bt222 = [GradientButton buttonWithType:UIButtonTypeCustom] ;
+//    [self.view addSubview:bt222] ;
+//    [bt222 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.width.equalTo(@100) ;
+//        make.height.equalTo(@40) ;
+//        make.center.equalTo(self.view) ;
+//    }] ;
+//    [bt222 setNeedsLayout] ;
+//    [bt222 layoutIfNeeded] ;
+//    [bt222 addColors:@[(__bridge id)[UIColor redColor].CGColor,(__bridge id)[UIColor yellowColor].CGColor]] ;
+//
+// ---------------------------------------------------------------- //
+
+#import <UIKit/UIKit.h>
+
+@interface GradientButton : UIButton
+
+- (void)addColors:(NSArray *)colors ;
+
+- (void)addColors:(NSArray *)colors bounds:(CGRect)bounds ;
+
+@end
---
+++ XTlib/XTlib/UIs/GradientButton/GradientButton.m
@@ -0,0 +1,45 @@
+//
+//  GradientButton.m
+//  SwiftCall
+//
+//  Created by teason23 on 2018/3/27.
+//  Copyright © 2018年 weyl. All rights reserved.
+//
+
+#import "GradientButton.h"
+
+@interface GradientButton ()
+@property (strong, nonatomic) CAGradientLayer *gradientLayer ;
+@end
+
+@implementation GradientButton
+
+- (void)addColors:(NSArray *)colors bounds:(CGRect)bounds {
+    if (self.gradientLayer.superlayer) [self.gradientLayer removeFromSuperlayer] ;
+    if (self.gradientLayer) self.gradientLayer = nil ;
+    
+    if ([[colors firstObject] isKindOfClass:[UIColor class]]) {
+        NSMutableArray *tmpColorList = [@[] mutableCopy] ;
+        [colors enumerateObjectsUsingBlock:^(UIColor *obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [tmpColorList addObject:(__bridge id)obj.CGColor] ;
+        }] ;
+        colors = [tmpColorList copy] ;
+    }
+    
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.colors = colors;
+    gradientLayer.locations = @[@0, @1.0];
+    gradientLayer.startPoint = CGPointMake(0, 0);
+    gradientLayer.endPoint = CGPointMake(1.0, 0);
+    gradientLayer.frame = bounds;
+    gradientLayer.cornerRadius = bounds.size.height / 2.;
+    self.gradientLayer = gradientLayer;
+    [self.layer insertSublayer:self.gradientLayer below:self.titleLabel.layer];
+
+}
+
+- (void)addColors:(NSArray *)colors {
+    [self addColors:colors bounds:self.bounds] ;
+}
+
+@end
