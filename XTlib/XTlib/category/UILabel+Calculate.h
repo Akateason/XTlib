20e8ce6f77aef1a25dfd39c6a2490833350b7735
teason <teason.xie@cootek.cn>
2018-04-25 18:32:32 +0800

    add

--- XTlib/XTlib/category/UILabel+Calculate.h
+++ XTlib/XTlib/category/UILabel+Calculate.h
@@ -10,18 +10,16 @@
 
 @interface UILabel (Calculate)
 
-//根据宽度求高度  content 计算的内容  width 计算的宽度 font字体大小
 + (CGFloat)getLabelHeightWithText:(NSString *)text
                             width:(CGFloat)width
                              font:(CGFloat)font ;
 
-//根据高度度求宽度  text 计算的内容  Height 计算的高度 font字体大小
 + (CGFloat)getLabelWidthWithText:(NSString *)text
                           height:(CGFloat)height
                             font:(CGFloat)font ;
 
-- (CGFloat)getLabelHeightWithWidth:(CGFloat)width ;
+- (CGFloat)caculateHeightWithWidth:(CGFloat)width ;
 
-- (CGFloat)getWidthWithHeight:(CGFloat)height ;
+- (CGFloat)caculateWidthWithHeight:(CGFloat)height ;
 
 @end
